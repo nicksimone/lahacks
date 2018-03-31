1df2569988682a151bcf0928a6123d12d43cee86
Nick Simone <nicksimone@Nicks-MacBook-Pro.local>
2018-03-31 13:51:52 -0700

    Dodgers js

---
+++ dodgers.js
@@ -0,0 +1,7 @@
+
+
+$(document).ready(function() {
+    $("#demo").html("Hello, World!");
+});
+
+$('h5').append('it works');
--- lakers.js
+++ lakers.js
@@ -1,1 +1,32 @@
-import json;
+const USERNAME = nsimone;
+const PASSWORD = sports1998;
+
+
+$(document).ready(function()
+{
+  console.log("It works");
+
+});
+// console.log("It works");
+
+
+
+function make_base_auth(user, password) {
+  var tok = user + ':' + password;
+  var hash = btoa(tok);
+  return "Basic " + hash;
+}
+$.ajax
+  ({
+    type: "GET",
+    url: "https://api.mysportsfeeds.com/v1.2/pull/nba/2016-2017-regular/overall_team_standings.json?teamstats=W,L,PTS,PTSA&team=105",
+    dataType: 'json',
+    async: false,
+    data: '{}',
+    beforeSend: function (xhr){
+        xhr.setRequestHeader('Authorization', make_base_auth(username, password));
+    },
+    success: function (){
+        alert('Thanks for your comment!');
+    }
+});
