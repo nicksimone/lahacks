1f86d619fd094910c517a243882445c883ece196
rysimone <ryansimone2324@gmail.com>
2018-03-31 15:30:23 -0700

    added API for Dodgers

--- dodgers.js
+++ dodgers.js
@@ -1,8 +1,58 @@
+const USERNAME = "nsimone";
+const PASSWORD = "sports1998";
+
+var dodgers;
+
+const url = "https://api.mysportsfeeds.com/v1.2/pull/nba/2016-2017-regular/overall_team_standings.json?teamstats=W,L,PTS,PTSA&team=105";
+
+// fetch(url)
+//   .then((resp) => resp.json())
+//   .then(function(data)
+//   {
+//     // Here you get the data to modify as you please
+//   })
+//   .catch(function(error) {
+//     // If there is any error you will catch them here
+//   });
+//
+// $(document).ready(function()
+// {
+//   setup();
+//   console.log("It works");
+// });
+//
+// function setup()
+// {
+//   loadJSON("https://api.mysportsfeeds.com/v1.2/pull/nba/2016-2017-regular/overall_team_standings.json?teamstats=W,L,PTS,PTSA&team=105", gotData);
+// }
+//
+// function gotData(data)
+// {
+//   lakers = data;
+// }
+//
+// function make_base_auth(user, password) {
+//   var tok = user + ':' + password;
+//   var hash = btoa(tok);
+//   return "Basic " + hash;
+// }
 
 
-$(document).ready(function() {
-    $("#demo").html("Hello, World!");
-    $('h5').append('it works');
+$.ajax({
+  type: "GET",
+  url: 'https://api.mysportsfeeds.com/v1.2/pull/mlb/2017-regular/overall_team_standings.json?teamstats=W,L,RF,RA&team=LAD',
+  dataType: 'json',
+  headers: {
+    "Authorization": "Basic " + btoa('nsimone' + ":" + 'sports1998')
+  },
+  success: function (data){
+    console.log('Got Data!');
+    console.log(data);
+    console.log(data.team['ID']);
+    },
+    error: function (xhr, ajaxOptions, thrownError) {
+        console.log('error');
+        alert(xhr.status);
+        alert(thrownError);
+     }
 });
-
-$('h5').append('it works');
